11ae086a3b7975c25e3dc6bc178c85209c2401fb
christiancvillaruz <42919026+christiancvillaruz@users.noreply.github.com>
2022-07-18 01:22:10 +0800

    updated app component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { Helmet } from "react-helmet";
+import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
+      <Helmet>
+        <title>PORTFOLIO | CHRISTIAN ROBERT C. VILLARUZ</title>
+      </Helmet>
       <Navbar/>
+      <Home/>
     </div>
   );
 }

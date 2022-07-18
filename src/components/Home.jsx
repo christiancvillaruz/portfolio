f15ff98c878a6e1081da3bd970c005db8717e2b5
christiancvillaruz <42919026+christiancvillaruz@users.noreply.github.com>
2022-07-18 13:53:02 +0800

    updated fonts and color palettes (components)

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,16 +3,16 @@ import { HiArrowNarrowRight } from  'react-icons/hi'
 
 const Home = () => {
   return (
-    <div name="home" className="w-full h-screen bg-stone-900">
+    <div name="home" className="w-full h-screen bg-home">
       { /* Container */ }
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
-        <p className="text-amber-300">Hi there, my name is</p>
-        <h1 className="text-4xl sm:text-7xl font-bold text-zinc-200 font-anton tracking-wide">Taylor</h1>
-        <h2 className="text-4xl sm:text-7xl font-bold text-zinc-400">I'm a Web Developer.</h2>
-        <p className="text-zinc-500 py-4 max-w-[700px]">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem odit rem labore, error voluptates est. Dolorum ipsam reiciendis dolor, fuga eum nostrum repudiandae animi voluptatem, amet, vel aspernatur est perspiciatis?</p>
+        <p className="text-subheadings text-3xl font-caveat">Hi there, my name is</p>
+        <h1 className="text-4xl sm:text-7xl uppercase text-headings font-abril">Christian Villaruz</h1>
+        <h2 className="text-4xl sm:text-6xl font-bold text-subheadings font-caveat">I'm a Web Developer.</h2>
+        <p className="text-paragraphs py-4 max-w-[700px]">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem odit rem labore, error voluptates est. Dolorum ipsam reiciendis dolor, fuga eum nostrum repudiandae animi voluptatem, amet, vel aspernatur est perspiciatis?</p>
         { /* Buttons */ }
         <div>
-          <button className="text-white group border-2 px-6 py-3 flex items-center hover:bg-amber-300 hover:border-amber-300 hover:text-zinc-900">My Projects 
+          <button className="text-subheadings group border-2 px-6 py-3 flex items-center hover:font-bold hover:bg-headings hover:border-headings hover:text-zinc-900">My Projects 
             <span className="group-hover:rotate-90 duration-300">
               <HiArrowNarrowRight className="ml-3" />
             </span>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,12 +10,9 @@ const Navbar = () => {
   const handleClick = () => setNav(!nav)
 
   return (
-    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-stone-900 text-gray-300">
-      <div>
-        <img src={ Logo } alt="logo" className="w-[50px]" />
-      </div>
+    <div className="fixed w-full h-[80px] flex justify-end items-center px-4 bg-home text-gray-300">
       { /* Navbar Items */ }
-      <ul className="hidden md:flex font-medium">
+      <ul className="hidden md:flex font-medium text-subheadings">
         <li>Home</li>
         <li>About</li>
         <li>Skills</li>
@@ -23,11 +20,11 @@ const Navbar = () => {
         <li>Contact</li>
       </ul>
       { /* Hamburger Menu */ }
-      <div className="md:hidden z-10" onClick={ handleClick }>
+      <div className="md:hidden z-10 text-subheadings" onClick={ handleClick }>
         { !nav ? <FaBars/> : <FaTimes/> }
       </div>
       { /* Responsive Menu (Mobile) */ }
-      <div className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-stone-900 flex flex-col justify-center items-center text-center'}>
+      <div className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-home text-subheadings flex flex-col justify-center items-center text-center'}>
         <ul>
           <li className="py-2 text-lg">Home</li>
           <li className="py-2 text-lg">About</li>
@@ -39,17 +36,17 @@ const Navbar = () => {
       { /* Social Media */ }
       <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
         <ul>
-          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-sky-900">
-            <a href="https://linkedin.com/in/christiancvillaruz" target="_blank" className="flex justify-between items-center w-full text-gray-300">LinkedIn<FaLinkedin size={30} /></a>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-accent">
+            <a href="https://linkedin.com/in/christiancvillaruz" target="_blank" rel="noreferrer" className="flex justify-between items-center w-full text-gray-300">LinkedIn<FaLinkedin size={30} /></a>
           </li>
-          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-black">
-            <a href="https://github.com/christiancvillaruz" target="_blank" className="flex justify-between items-center w-full text-gray-300">GitHub<FaGithub size={30} /></a>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-accent">
+            <a href="https://github.com/christiancvillaruz" target="_blank" rel="noreferrer" className="flex justify-between items-center w-full text-gray-300">GitHub<FaGithub size={30} /></a>
           </li>
-          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600">
-            <a href="https://facebook.com/kristyaaaaan7" target="_blank" className="flex justify-between items-center w-full text-gray-300">Facebook<FaFacebook size={30} /></a>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-accent">
+            <a href="https://facebook.com/kristyaaaaan7" target="_blank" rel="noreferrer" className="flex justify-between items-center w-full text-gray-300">Facebook<FaFacebook size={30} /></a>
           </li>
-          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-cyan-500">
-            <a href="https://twitter.com/robertssson13" target="_blank" className="flex justify-between items-center w-full text-gray-300">Twitter<FaTwitter size={30} /></a>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-accent">
+            <a href="https://twitter.com/robertssson13" target="_blank" rel="noreferrer" className="flex justify-between items-center w-full text-gray-300">Twitter<FaTwitter size={30} /></a>
           </li>
         </ul>
       </div>

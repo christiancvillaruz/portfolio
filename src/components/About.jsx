f0c3bfef8586843f6125184ad7dd10f62498a21a
christiancvillaruz <42919026+christiancvillaruz@users.noreply.github.com>
2022-07-20 00:16:39 +0800

    updated about component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs'
+import 'react-tabs/style/react-tabs.css';
 import GradPic1 from '../assets/gradpic-1.png'
+import QCULogo from '../assets/qcu-logo-lg.jpg'
+import SVDPALogo from '../assets/svdpa-logo-lg.jpg'
 
 const About = () => {
   return (
@@ -23,6 +27,121 @@ const About = () => {
             <h3 className="mt-4 text-justify indent-10 sm:text-left sm:indent-0">
               Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse pariatur architecto repudiandae! Similique reiciendis rem facilis iste rerum inventore debitis assumenda porro quam eligendi! Dolores recusandae dolorem similique odio eum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, atque nam dolor iure soluta perspiciatis asperiores nostrum aperiam a vel animi commodi obcaecati, ad molestiae fuga, harum distinctio nemo exercitationem.
             </h3>
+          </div>
+        </div>
+        <div className="flex flex-col justify-center items-center w-full h-full py-20">
+          <div className="max-w-[1000px] w-full p-4">
+            <Tabs>
+              <TabList>
+                <Tab>Education</Tab>
+                <Tab>Interests</Tab>
+              </TabList>
+
+              <TabPanel>
+                <div className="py-4">
+                  <h2 className="text-center text-4xl text-white font-bebas py-4">Education Timeline</h2>
+                  <div className="flex justify-center items-center p-0">
+                    <div className="max-w-7xl mx-auto w-full grid grid-cols-9">
+
+                      {/* First */}
+                      {/* Container */}
+                      <div className="col-span-4 w-full h-full pr-4 text-sm sm:text-base sm:pr-0">
+                        <div className="w-full h-full bg-gradient-to-r from-[#557C55] to-[#82aa74] rounded-md text-white">
+
+                          <div className="flex flex-row justify-around">
+                            <div className="hidden sm:grid overflow-hidden">
+                              <img src={QCULogo} alt="qcu logo" className="w-[19rem] rounded-full ml-[-70px] mt-12 object-cover scale-[1.58] rotate-45 grayscale opacity-80"/>
+                            </div>
+
+                            <div className="p-4">
+                              <h1 className="text-white text-right font-caveat text-4xl pb-1">College</h1>
+                              <div className="text-white text-right">
+                                <h3>Bachelor of Science in Information Technology</h3>
+                                <h1 className="text-3xl font-bebas mt-3">Quezon City University</h1>
+                                <p>Quezon City, Metro Manila</p>
+                                <p className="italic mt-3">Magna Cum Laude Awardee</p>
+                                <p>GWA: 1.42</p>
+                              </div>
+                            </div>
+                          </div>
+
+                        </div>
+                      </div>
+                      {/* Line */}
+                      <div className="relative col-span-1 w-full h-full flex justify-center items-center text-sm sm:text-base">
+                        <div className="h-full w-1 bg-headings py-20"></div>
+                        <div className="absolute p-1 rounded-md bg-headings z-1 text-zinc-900 font-bold text-center">2018-2022</div>
+                      </div>
+                      <div className="col-span-4 w-full h-full"></div>
+
+                      {/* Second */}
+                      {/* Line */}
+                      <div className="col-span-4 w-full h-full"></div>
+                      <div className="relative col-span-1 w-full h-full flex justify-center items-center text-sm sm:text-base">
+                        <div className="h-full w-1 bg-headings py-20"></div>
+                        <div className="absolute p-1 rounded-md bg-headings z-1 text-zinc-900 font-bold text-center">2016-2018</div>
+                      </div>
+                      {/* Container */}
+                      <div className="col-span-4 w-full h-full pr-4 text-sm sm:text-base sm:pr-0">
+                        <div className="w-full h-full bg-gradient-to-r from-[#557C55] to-[#82aa74] rounded-md text-white">
+
+                          <div className="flex flex-row-reverse justify-around">
+                            <div className="hidden sm:grid overflow-hidden">
+                              <img src={SVDPALogo} alt="svdpa logo" className="w-[19rem] rounded-full mr-[-6rem] ml-[70px] mt-[3.6rem] object-cover scale-[1.7] -rotate-45 grayscale opacity-80"/>
+                            </div>
+
+                            <div className="p-4">
+                              <h1 className="text-white text-left font-caveat text-4xl pb-1">Senior High</h1>
+                              <div className="text-white text-left">
+                                <h3>Science, Technology, Engineering, and Mathematics</h3>
+                                <h1 className="text-2xl font-bebas mt-3">St. Vincent de Paul Academy of Novaliches, Inc.</h1>
+                                <p>Quezon City, Metro Manila</p>
+                                <p className="italic mt-3">Class Salutatorian Awardee</p>
+                                <p className="italic">Loyalty Awardee</p>
+                              </div>
+                            </div>
+                          </div>
+
+                        </div>
+                      </div>
+
+                      {/* Third */}
+                      {/* Container */}
+                      <div className="col-span-4 w-full h-full pr-4 text-sm sm:text-base sm:pr-0">
+                        <div className="w-full h-full bg-gradient-to-r from-[#557C55] to-[#82aa74] rounded-md text-white">
+
+                          <div className="flex flex-row justify-around">
+                            <div className="hidden sm:grid overflow-hidden">
+                              <img src={SVDPALogo} alt="qcu logo" className="w-[19rem] rounded-full ml-[-70px] mt-6 object-cover scale-[1.3] rotate-45 grayscale opacity-80"/>
+                            </div>
+
+                            <div className="p-4">
+                              <h1 className="text-white text-right font-caveat text-4xl pb-1">High School</h1>
+                              <div className="text-white text-right">
+                                <h1 className="text-2xl font-bebas">St. Vincent de Paul Academy of Novaliches, Inc.</h1>
+                                <p>Quezon City, Metro Manila</p>
+                                <p className="italic mt-3">Consistent Honor Student</p>
+                              </div>
+                            </div>
+                          </div>
+
+                        </div>
+                      </div>
+                      {/* Line */}
+                      <div className="relative col-span-1 w-full h-full flex justify-center items-center text-sm sm:text-base">
+                        <div className="h-full w-1 bg-headings py-20"></div>
+                        <div className="absolute p-1 rounded-md bg-headings z-1 text-zinc-900 font-bold text-center">2018-2022</div>
+                      </div>
+                      <div className="col-span-4 w-full h-full"></div>
+                      
+                    </div>
+                  </div>
+                </div>
+              </TabPanel>
+              <TabPanel>
+                <h2>Interests</h2>
+              </TabPanel>
+            </Tabs>
           </div>
         </div>
       </div>

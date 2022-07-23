1ef479e20ebf53eb1acbe462745439a1097d2a10
christiancvillaruz <42919026+christiancvillaruz@users.noreply.github.com>
2022-07-24 00:42:35 +0800

    updated app component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -15,7 +17,9 @@ function App() {
       <Home/>
       <About/>
       <Skills/>
-      <Projects />
+      <Projects/>
+      <Contact/>
+      <Footer/>
     </div>
   );
 }

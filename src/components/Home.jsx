d38ae447424267c5332d30df32e438dcad9b3b6d
christiancvillaruz <42919026+christiancvillaruz@users.noreply.github.com>
2022-07-24 00:43:16 +0800

    updated home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
         <p className="text-subheadings text-3xl font-caveat">Hi there, my name is</p>
         <h1 className="text-4xl sm:text-7xl uppercase text-headings font-abril">Christian Villaruz</h1>
         <h2 className="text-4xl sm:text-6xl font-bold text-subheadings font-caveat">I'm a Web Developer.</h2>
-        <p className="text-paragraphs py-4 max-w-[700px]">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem odit rem labore, error voluptates est. Dolorum ipsam reiciendis dolor, fuga eum nostrum repudiandae animi voluptatem, amet, vel aspernatur est perspiciatis?</p>
+        <p className="text-paragraphs py-4 max-w-[700px]">I'm an ambitious, driven, and passionate person. Graduated Magna Cum Laude from Quezon City University with a degree in Bachelor of Science in Information Technology and currently pursuing real-world experience to develop acquired skills.</p>
         { /* Buttons */ }
         <div className="flex flex-row gap-3">
           <button className="text-subheadings group border-2 px-6 py-3 rounded-md flex items-center hover:font-bold hover:bg-headings hover:border-headings hover:text-zinc-900">My Projects 
